394c5aa11b65d02a0f808ae5143c353b8e97baea
littlenines <nemanja584@hotmail.com>
2024-10-07 01:53:25 +0200

    change cube light intensity

--- src/components/CubePortal/index.jsx
+++ src/components/CubePortal/index.jsx
@@ -17,9 +17,9 @@ const CubePortal = () => {
 
     return (
         <Canvas resize gl={{ preserveDrawingBuffer: true }} camera={{ near: 0.1, far: 1000 }} className="canvas_cube">
-            <ambientLight intensity={0.7} />
-            <directionalLight position={[5, 12, 0]} color={0xB275FB} intensity={0.6} />
-            <directionalLight position={[7, 12, 0]} color={0x4AC0FF} intensity={0.5} />
+            <ambientLight intensity={0.8} />
+            <directionalLight position={[5, 12, 0]} color={0xB275FB} intensity={0.7} />
+            <directionalLight position={[7, 12, 0]} color={0x4AC0FF} intensity={0.6} />
             <OrbitControls target={[0, 0, 4]} enablePan={false} enableZoom={false} />
             <Float speed={2} floatIntensity={2}>
                 <CubeNormal {...headProps1} />
