3f1841bc8c56d4b34a5bd8d4d6a5b1c1421ffdff
littlenines <nemanja584@hotmail.com>
2024-11-18 12:52:53 +0100

    change loader

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -3,23 +3,9 @@ import styles from './Loader.module.scss'
 
 const Loader = () => {
   return (
-    <>
-      <div className={styles.wrapper}>
-        <div className={styles.ball}></div>
-        <div className={styles.ball}></div>
-        <div className={styles.ball}></div>
-      </div>
-
-      <svg>
-        <defs>
-          <filter id="filter">
-            <feGaussianBlur in="SourceGraphic" stdDeviation="18" result="blur" />
-            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 28 -10" result="filter" />
-            <feComposite in="SourceGraphic" in2="filter" operator="atop" />
-          </filter>
-        </defs>
-      </svg>
-    </>
+    <div className={styles['loader-container']}>
+      <div className={styles.loader}></div>
+    </div>
   )
 }
 
