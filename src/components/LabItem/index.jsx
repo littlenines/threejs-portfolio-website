acef9078d13cc099b394a3992f79f1adbe04972a
littlenines <nemanja584@hotmail.com>
2025-02-08 12:33:52 +0100

    fix image stutter

--- src/components/LabItem/index.jsx
+++ src/components/LabItem/index.jsx
@@ -8,7 +8,7 @@ const LabItem = ({image, imageAlt, title, subtitle, link, ...props}) => {
     <div {...props}>
       <div className={styles.lab_image}>
         <Link to={link}>
-          <img src={image} alt={imageAlt} loading='lazy' />
+          <img src={image} alt={imageAlt} loading='lazy' decoding="async"/>
         </Link>
       </div>
       <div className={styles.lab_info}>
