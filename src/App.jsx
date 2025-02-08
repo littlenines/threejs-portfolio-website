ac78c8d06b44b847ee9ffa4d632f9820596678a2
littlenines <nemanja584@hotmail.com>
2025-02-09 00:26:50 +0100

    refactor global scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { lazy, useEffect, useState } from "react";
+import { lazy, useEffect, useState, Suspense } from "react";
 import { useProgress, useGLTF } from "@react-three/drei";
 import { motion } from "motion/react";
-import WheatleyGLTF from './assets/3D/portal.glb';
-const Section = lazy(() => import("./components/Section"));
-const Header = lazy(() => import("./components/Layout/Header"));
-const LiquidCircle = lazy(() => import("./components/LiquidCircle"));
+import Loader from "./components/Loader";
 const HeroInfo = lazy(() => import("./components/HeroInfo"));
 const HeroButtons = lazy(() => import("./components/HeroButtons"));
-const Loader = lazy(() => import("./components/Loader"));
+import WheatleyGLTF from './assets/3D/portal.glb';
+const LiquidCircle = lazy(() => import("./components/LiquidCircle"));
+const Header = lazy(() => import("./components/Layout/Header"));
+const Section = lazy(() => import("./components/Section"));
 const LabItem = lazy(() => import("./components/LabItem"));
 const CubePortal = lazy(() => import("./components/CubePortal"));
 const MemoCard = lazy(() => import("./components/MemoCard"));
 const ContactMe = lazy(() => import("./components/ContactMe"));
-import projects from "./assets/json/projects.json"
-import skills from './assets/json/skills.json'
+import projects from "./assets/json/projects.json";
+import skills from './assets/json/skills.json';
 
-import "./App.scss";
+import styles from './App.module.scss';
 
 function App() {
   useGLTF.preload(WheatleyGLTF, true, true);
@@ -25,7 +25,7 @@ function App() {
 
   useEffect(() => {
     if (progress !== 100) return;
-    const appReadyTimeout = setTimeout(() => setAppReady(true), 250);
+    const appReadyTimeout = setTimeout(() => setAppReady(true), 0);
     
     return () => clearTimeout(appReadyTimeout)
   }, [progress]);
@@ -33,10 +33,10 @@ function App() {
   if (!isAppReady) return <Loader aria-live="polite" />;
 
   return (
-    <>
+    <Suspense fallback={<Loader aria-live="polite" />}>
       <Header />
       <Section className="container--hero" id='about'>
-        <motion.div className="hero"
+        <motion.div className={styles.hero}
                     initial={{ opacity: 0, transform: 'translate3d(0, 125px, 0)' }}
                     whileInView={{ opacity: 1, transform: 'translate3d(0, 0, 0)' }}
                     viewport={{ once: true }}
@@ -45,7 +45,7 @@ function App() {
             <HeroInfo title='Hey! My name is Nemanja.'
                       typewrite='FRONTEND DEVELOPER'
                       description='I&apos;m from Serbia, currently based in Niš. With over 3 years of experience, focused on writing clean, efficient code to bring web projects to life. I’m passionate about developing interactive and responsive user interfaces and always up for tackling new challenges in frontend development.' />
-            <div className="hero_info_buttons">
+            <div className={styles.hero_info_buttons}>
               <HeroButtons github='https://github.com/littlenines' 
                            linkedin='https://www.linkedin.com/in/galbinovic584/'
                            cv='/cv/nemanja-galbinovic-cv.pdf' />
@@ -60,11 +60,11 @@ function App() {
 
       {/* Personal Projects */}
       <Section title='Personal Projects' id='projects'>
-        <div className="projects_card">
+        <div className={styles.projects_card}>
           {projects.map(({ image, imageAlt, title, subtitle, slug }) => {
             return (
               <motion.section key={slug}
-                              className='projects_card_item'
+                              className={styles.projects_card_item}
                               initial={{ opacity: 0, translateY: '150px' }}
                               whileInView={{ opacity: 1, translateY: 0 }}
                               viewport={{ once: true }}
@@ -82,7 +82,7 @@ function App() {
 
       {/* Skills */}
       <Section title='Technologies I&apos;ve Been Working With' className="container--medium" id='technology'>
-        <div className="skills_wrap">
+        <div className={styles.skills_wrap}>
           {skills?.map(({ id, image, alt, text }) => {
             return (
               <motion.section key={id}
@@ -100,7 +100,7 @@ function App() {
       <Section title='Want to work with me?' id='contact'>
         <ContactMe title='Send me a message' mail='galbinovic584@gmail.com' />
       </Section>
-    </>
+    </Suspense>
   );
 }
 
